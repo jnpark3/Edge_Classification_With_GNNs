d65fbb333f5b64b25ed948a6b2618284590bef73
Jian Park <jianpark@Jians-MacBook-Pro.local>
2023-03-09 23:00:37 -0600

    Added visualizations and creation of utils folder to store classes

---
+++ task_1/utils/myDataset.py
@@ -0,0 +1,17 @@
+from torch.utils.data import Dataset
+from pathlib import Path
+import torch
+
+class MyDataset(Dataset):
+    def __init__(self, path: Path):
+        super().__init__()
+        self.graphs = list(path.glob("*.pt"))
+    
+    def append(self, path: Path):
+        self.graphs = self.graphs + list(path.glob("*.pt"))
+
+    def __getitem__(self, idx):
+        return torch.load(self.graphs[idx])
+    
+    def __len__(self) -> int:
+        return len(self.graphs)
